2db141a2acc78f8c0dd6cae2c8e7251edd4dd11f
Koliveir <erickoliveira.sp@gmail.com>
2021-02-06 23:03:40 -0300

    Adicionei uma linha no log que informa quantos graus estava no momento da troca do wallpaper

--- script.sh
+++ script.sh
@@ -8,7 +8,8 @@ FRIO=$HOME/$CAMINHO/frio.jpg
 NORMAL=$HOME/$CAMINHO/normal.jpg
 QUENTE=$HOME/$CAMINHO/quente.jpg
 M_QUENTE=$HOME/$CAMINHO/muito_quente.jpg
-TEMPO=$(curl wttr.in/sao-paulo?format=3 | cut -d'+' -f2 | cut -c 1,2)
+TEMPO=$(curl https://wttr.in/?format=%l:+%t | cut -d':' -f2 | cut -c 3,4)
+MTempo=$TEMPO
 HORA=$(date)
 
 #CRIACAO DO LOG
@@ -16,24 +17,28 @@ HORA=$(date)
 echo $TEMPO >> $HOME/$CAMINHO/../log.txt
 echo $HORA >> $HOME/$CAMINHO/../log.txt
 
-
 #VERIFICACAO DO TEMPO ATUAL E APLICACAO DO WALLPAPER
 
 if [ $TEMPO -lt 10 ];then
     gsettings set org.gnome.desktop.background picture-uri file://$M_FRIO
     echo "gsettings set org.gnome.desktop.background picture-uri file://$M_FRIO" >> $HOME/$CAMINHO/../log.txt
+    echo "A temperatura estava $MTempo graus" >> $HOME/$CAMINHO/../log.txt
 elif [ $TEMPO -lt 20 ];then
     gsettings set org.gnome.desktop.background picture-uri file://$FRIO
     echo "gsettings set org.gnome.desktop.background picture-uri file://$FRIO" >> $HOME/$CAMINHO/../log.txt
+    echo "A temperatura estava $MTempo graus" >> $HOME/$CAMINHO/../log.txt
 elif [ $TEMPO -lt 25 ];then
     gsettings set org.gnome.desktop.background picture-uri file://$NORMAL
     echo "gsettings set org.gnome.desktop.background picture-uri file://$NORMAL" >> $HOME/$CAMINHO/../log.txt
+    echo "A temperatura estava $MTempo graus" >> $HOME/$CAMINHO/../log.txt
 elif [ $TEMPO -lt 29 ];then
     gsettings set org.gnome.desktop.background picture-uri file://$QUENTE
     echo "gsettings set org.gnome.desktop.background picture-uri file://$QUENTE" >> $HOME/$CAMINHO/../log.txt
+    echo "A temperatura estava $MTempo graus" >> $HOME/$CAMINHO/../log.txt
 else
     gsettings set org.gnome.desktop.background picture-uri file://$M_QUENTE
     echo "gsettings set org.gnome.desktop.background picture-uri file://$M_QUENTE" >> $HOME/$CAMINHO/../log.txt
+    echo "A temperatura estava $MTempo graus" >> $HOME/$CAMINHO/../log.txt
 fi
 
 exit
